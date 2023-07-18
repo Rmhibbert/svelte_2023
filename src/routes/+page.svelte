d6027194438eb30d6d42273e950fa6e236d6ec37
Rory Hibbert <hibbrm1@student.op.ac.nz>
2023-07-19 09:32:43 +1200

    Linked pt2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
-<link href= "style.css">
+<head>
+<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
