b653db5d9c763f33e1d61dd04af68ba647e17a78
Rory Hibbert <hibbrm1@student.op.ac.nz>
2023-08-04 10:05:32 +1200

    edited the nav

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,5 +1,5 @@
 <nav>    
-    <li ><a href = "http://localhost:5173/page1" target= _blank>Page1</a></li>
-    <li> <a href = "http://localhost:5173/page2" target= _blank>Page 2</a></li>
-    <li> <a href = "https://www.nzherald.co.nz/" target= _blank>NZ HERALD</a></li>
+    <a href="http://localhost:5173/">Home |</a>
+    <a href = "http://localhost:5173/page1" >Page1 |</a>
+    <a href = "http://localhost:5173/page2" >Page 2 </a>    
 </nav>
