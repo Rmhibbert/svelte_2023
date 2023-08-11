5c58d6758f7de2b115aaf4b790a4006a981f5b52
Rory Hibbert <hibbrm1@student.op.ac.nz>
2023-08-11 12:00:31 +1200

    completed week 4 tasks

---
+++ src/routes/week4/+page.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+    <link rel="stylesheet" href="css/main.css" />
+</svelte:head>
+
+
+<div>
+    <div></div>
+    <div></div>
+    <div>
+      <div>Style this one</div>
+    </div>
+    <div>
+      <div>But don't style this one!</div>
+    </div>
+  </div>
